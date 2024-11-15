30c558d342bc1bd7e4412bbd5204ed725a75ef68
Likenesio <valentinubb@gmail.com>
2024-11-15 18:59:25 -0300

    first commit

---
+++ evertecdemo/src/main/java/com/example/evertecdemo/EvertecdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.evertecdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertecdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvertecdemoApplication.class, args);
+	}
+
+}
---
+++ evertecdemo/src/test/java/com/example/evertecdemo/EvertecdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evertecdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvertecdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
